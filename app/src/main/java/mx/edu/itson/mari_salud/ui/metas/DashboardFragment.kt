123b874a7a80318fcc5e875e96e2d9414f1790a6
alfonsosempoalt <alfonsosempoalt@hotmail.com>
2021-04-10 17:06:31 -0600

    como te sientes layout y menu fragments

---
+++ app/src/main/java/mx/edu/itson/mari_salud/ui/metas/DashboardFragment.kt
@@ -0,0 +1,31 @@
+package mx.edu.itson.mari_salud.ui.metas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import mx.edu.itson.mari_salud.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_metas, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
