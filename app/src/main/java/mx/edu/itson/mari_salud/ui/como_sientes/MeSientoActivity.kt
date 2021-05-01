95f1f020c029c042471dcd7422fbf8ea39f342b0
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-05-01 00:03:46 -0600

    comoTeSientesLayout

--- app/src/main/java/mx/edu/itson/mari_salud/ui/como_sientes/MeSientoActivity.kt
+++ app/src/main/java/mx/edu/itson/mari_salud/ui/como_sientes/MeSientoActivity.kt
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_me_siento.*
+import kotlinx.android.synthetic.main.activity_mis_estados.*
 import mx.edu.itson.mari_salud.R
 import java.time.LocalDateTime
 
 class MeSientoActivity : AppCompatActivity() {
+    var lstEstados = ArrayList<Estado>()
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +23,16 @@ class MeSientoActivity : AppCompatActivity() {
             finish()
         }
 
-        tv_fecha_hoySiento.text =current.dayOfMonth.toString()+" de "+current.month.toString()
+        tv_fecha_hoySiento.text = current.dayOfMonth.toString() + " de " + current.month.toString()
+
+        lstEstados.add(Estado("Estado 1", false))
+        lstEstados.add(Estado("Estado 2", false))
+        lstEstados.add(Estado("Estado 3", false))
+        var adapter = EditorEstadoLVAdapter(lstEstados, this)
+        lv_estados.adapter = adapter
 
         lytAniadirEstado.setOnClickListener {
-            var intent=Intent(this,MisEstadosActivity::class.java)
+            var intent = Intent(this, MisEstadosActivity::class.java)
             startActivity(intent)
         }
 
