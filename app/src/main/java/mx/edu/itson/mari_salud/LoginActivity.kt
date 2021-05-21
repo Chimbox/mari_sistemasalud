0543e6e63a4fc6e1e81c47cec73c47565ccda3f9
chimbox <chimboxsoftgve@gmail.com>
2021-05-21 03:03:33 -0600

    Se agregó autenticación y almacenamiento con Firebase

--- app/src/main/java/mx/edu/itson/mari_salud/LoginActivity.kt
+++ app/src/main/java/mx/edu/itson/mari_salud/LoginActivity.kt
@@ -3,17 +3,26 @@ package mx.edu.itson.mari_salud
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.etEmail
+import kotlinx.android.synthetic.main.activity_login.etPassword
+import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         btnEntrar.setOnClickListener {
-            var intent=Intent(this,WelcomeActivity::class.java)
-            startActivity(intent)
-            finish()
+            if(!etEmail.text.toString().isNullOrBlank()&&!etPassword.text.toString().isNullOrBlank()){
+                login(etEmail.text.toString(), etPassword.text.toString())
+            }else{
+                Toast.makeText(this, "Los campos no pueden quedar vacíos.", Toast.LENGTH_SHORT).show()
+            }
         }
 
         tvRegistrate.setOnClickListener {
@@ -21,5 +30,18 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun login(email:String, password:String){
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this) {
+                if(it.isSuccessful){
+                    var intent=Intent(this,WelcomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    Toast.makeText(this, "Ha ocurrido un error al iniciar sesión, intente de nuevo.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
