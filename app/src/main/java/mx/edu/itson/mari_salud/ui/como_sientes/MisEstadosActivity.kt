144b48ebf94a885a7c8743d2a238a7de2afd3a0b
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-05-26 18:54:54 -0600

    agrega estados persanlizados pero da error al cargarlos, y en el apartado emergencia hace el dial de llamada

--- app/src/main/java/mx/edu/itson/mari_salud/ui/como_sientes/MisEstadosActivity.kt
+++ app/src/main/java/mx/edu/itson/mari_salud/ui/como_sientes/MisEstadosActivity.kt
@@ -2,7 +2,12 @@ package mx.edu.itson.mari_salud.ui.como_sientes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_mis_estados.*
+import mx.edu.itson.mari_salud.MenuActivity
 import mx.edu.itson.mari_salud.R
 import mx.edu.itson.mari_salud.ui.dominio.Estado
 
@@ -10,16 +15,28 @@ import mx.edu.itson.mari_salud.ui.dominio.Estado
 class MisEstadosActivity : AppCompatActivity() {
 
     var lstEstados = ArrayList<Estado>()
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private var adapter: EditorEstadoLVAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mis_estados)
-        lstEstados.add(Estado("EstadoAnimo 1", false))
-        lstEstados.add(Estado("EstadoAnimo 2", false))
-        lstEstados.add(Estado("EstadoAnimo 3", false))
-        var adapter = EditorEstadoLVAdapter(lstEstados, this)
-        lvEstados.adapter = adapter
 
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        db.collection("perfil")
+            .document(MenuActivity.idDocumentoPerfil)
+            .get()
+            .addOnSuccessListener { it ->
+                var lstStringEstadosPersonalizado =
+                    it.get("estados_personalizado") as ArrayList<Estado>
+                lstEstados = lstStringEstadosPersonalizado
+                Toast.makeText(this, lstEstados.toString(), Toast.LENGTH_LONG).show()
+                adapter = EditorEstadoLVAdapter(lstEstados, this)
+                lvEstados.adapter = adapter
+            }
 
         btnRegresarEstado.setOnClickListener {
             finish()
@@ -27,7 +44,16 @@ class MisEstadosActivity : AppCompatActivity() {
         btnAgregarEstado.setOnClickListener {
             lstEstados.add(Estado(etEstado.text.toString(), false))
             etEstado.text.clear()
-            adapter.notifyDataSetChanged()
+            adapter?.notifyDataSetChanged()
+            actualizaEstadosPersonalizado()
         }
+
     }
+
+    private fun actualizaEstadosPersonalizado() {
+        db.collection("perfil")
+            .document(MenuActivity.idDocumentoPerfil)
+            .update("estados_personalizado", lstEstados)
+    }
+
 }
--- app/src/main/java/mx/edu/itson/mari_salud/ui/emergencia/NecesitoAyudaActivity.kt
+++ app/src/main/java/mx/edu/itson/mari_salud/ui/emergencia/NecesitoAyudaActivity.kt
@@ -1,18 +1,75 @@
 package mx.edu.itson.mari_salud.ui.emergencia
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_necesito_ayuda.*
 import mx.edu.itson.mari_salud.R
 
 class NecesitoAyudaActivity : AppCompatActivity() {
+    private var STORAGE_PERMISSION_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_necesito_ayuda)
-
         btnRegresar_necesitoAyuda.setOnClickListener {
             finish()
         }
+        btn_necesitoAtencion_NecesitoAyuda.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+              call()
+            } else {
+                requestCallPermission()
+            }
+        }
     }
+
+    private fun call() {
+        var intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:911"))
+        startActivity(intent)
+
+    }
+
+    private fun requestCallPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CALL_PHONE
+            )
+        ) {
+            Toast.makeText(this, "Activar permisos desde ajustes", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CALL_PHONE),
+                STORAGE_PERMISSION_CODE
+            )
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                call()
+            } else {
+                Toast.makeText(this, "permisos rechazados", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
